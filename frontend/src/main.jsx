962b46ffede842e7625606192040adb9f7e83440
tuongroth <44430600+tuongroth@users.noreply.github.com>
2025-12-17 07:03:22 -0800

    OK
    OK

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './context/Storecontext.jsx'
+import StoreContextProvider from './context/StoreContext.jsx'
 console.log("React is starting...");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
